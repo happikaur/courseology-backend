fd69082b9c7ef0aab9a9f5312bdcee3021a43849
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-03-08 12:14:29 +0000

    Made a maven project. Connected the SQL Workbench. Made sure the post or get or delete request works in postman.

---
+++ src/main/java/com/nology/demo/Course.java
@@ -0,0 +1,56 @@
+package com.nology.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String courseName;
+    private String location;
+    private double price;
+    private String duration;
+    private String summary;
+
+
+    public Course(String courseName, String location, double price, String duration, String summary) {
+        this.courseName = courseName;
+        this.location = location;
+        this.price = price;
+        this.duration = duration;
+        this.summary = summary;
+    }
+
+    public Course() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+}
